a314d028cb1027d4f9445480ad7830249f69e815
bifidokk <bifidokk@gmail.com>
2023-10-14 10:41:55 +0300

    Add 404, 405 handlers

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	"github.com/bifidokk/go-api/internal/api"
 	"github.com/bifidokk/go-api/internal/config"
 	"github.com/bifidokk/go-api/internal/middleware"
@@ -8,6 +10,18 @@ import (
 )
 
 func RegisterRoutes(router *gin.Engine, conf *config.Config) {
+	router.NoRoute(func(c *gin.Context) {
+		c.JSON(http.StatusNotFound, gin.H{
+			"message": "404 Not found",
+		})
+	})
+
+	router.NoMethod(func(c *gin.Context) {
+		c.JSON(http.StatusMethodNotAllowed, gin.H{
+			"message": "405 Method not allowed",
+		})
+	})
+
 	apiV1Group := router.Group("/api")
 	publicGroup := router.Group("/public")
 
