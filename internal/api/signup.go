5dd066b3e0c4a59a9d0b18b0af382a1a6f35cfac
bifidokk <bifidokk@gmail.com>
2023-10-23 18:43:37 +0300

    Fix env, remove auth from validation

--- internal/api/signup.go
+++ internal/api/signup.go
@@ -6,15 +6,13 @@ import (
 
 	"github.com/bifidokk/go-api/internal/config"
 	"github.com/bifidokk/go-api/internal/repository"
-	"github.com/bifidokk/go-api/internal/service/auth"
 	"github.com/bifidokk/go-api/internal/service/signup"
 	"github.com/gin-gonic/gin"
 )
 
 func Signup(router *gin.RouterGroup, conf *config.Config) {
 	var userRepository = repository.NewUserRepository(conf.Db())
-	var authService = auth.NewAuth(userRepository, conf.Env.JwtSecret, int(conf.Env.JwtTtl))
-	var signupService = signup.NewSignup(userRepository, authService)
+	var signupService = signup.NewSignup(userRepository)
 
 	router.POST("/signup", func(c *gin.Context) {
 		var request signup.SignupRequest
--- internal/service/signup/signup.go
+++ internal/service/signup/signup.go
@@ -3,23 +3,20 @@ package signup
 import (
 	"github.com/bifidokk/go-api/internal/entity"
 	"github.com/bifidokk/go-api/internal/repository"
-	"github.com/bifidokk/go-api/internal/service/auth"
 	"golang.org/x/crypto/bcrypt"
 )
 
 type signup struct {
 	userRepository repository.UserRepository
-	auth           auth.Auth
 }
 
 type Signup interface {
 	CreateUser(request SignupRequest) (entity.User, error)
 }
 
-func NewSignup(userRepository repository.UserRepository, auth auth.Auth) Signup {
+func NewSignup(userRepository repository.UserRepository) Signup {
 	return &signup{
 		userRepository: userRepository,
-		auth:           auth,
 	}
 }
 
