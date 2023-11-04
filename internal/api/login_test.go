53879d24ed55e31929c3e1917bbf6e3b588aabdb
bifidokk <bifidokk@gmail.com>
2023-11-04 18:30:35 +0200

    Add more login tests

--- internal/api/login_test.go
+++ internal/api/login_test.go
@@ -9,6 +9,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type InvalidLoginRequest struct {
+    Email string `json:"email"`
+}
+
 func TestLogin(t *testing.T) {
 	t.Run("successful login", func(t *testing.T) {
 		app, router, conf := NewApiTest()
@@ -22,5 +26,46 @@ func TestLogin(t *testing.T) {
 		r := PerformRequestWithBody(app, "POST", "/public/login", string(body))
 
 		assert.Equal(t, http.StatusOK, r.Code)
+	})
+
+	t.Run("failed login with wrong credentials", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		Login(router, conf)
+
+		body, _ := json.Marshal(auth.LoginRequest{
+			Email:    "non-existing-user@test.com",
+			Password: "123456!",
+		})
+
+		r := PerformRequestWithBody(app, "POST", "/public/login", string(body))
+
+		assert.Equal(t, http.StatusUnauthorized, r.Code)
+	})
+
+	t.Run("failed login with wrong password", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		Login(router, conf)
+
+		body, _ := json.Marshal(auth.LoginRequest{
+			Email:    "user@test.com",
+			Password: "wrong password",
+		})
+
+		r := PerformRequestWithBody(app, "POST", "/public/login", string(body))
+
+		assert.Equal(t, http.StatusUnauthorized, r.Code)
+	})
+
+	t.Run("failed login without required fields", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		Login(router, conf)
+
+		body, _ := json.Marshal(InvalidLoginRequest{
+			Email:    "non-existing-user@test.com",
+		})
+
+		r := PerformRequestWithBody(app, "POST", "/public/login", string(body))
+
+		assert.Equal(t, http.StatusUnprocessableEntity, r.Code)
 	})
 }
