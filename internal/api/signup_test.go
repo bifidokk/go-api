caa2e010c5497db742f9259f039b9f14f472a172
bifidokk <bifidokk@gmail.com>
2023-11-06 18:51:35 +0200

    Add signup test

---
+++ internal/api/signup_test.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+	"testing"
+
+	"github.com/bifidokk/go-api/internal/service/signup"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSignUp(t *testing.T) {
+	t.Run("successful signup", func(t *testing.T) {
+		app, router, conf := NewApiTest()
+		Signup(router, conf)
+
+		body, _ := json.Marshal(signup.SignupRequest{
+			Email:    "new-user@test.com",
+			Password: "123456!",
+		})
+
+		r := PerformRequestWithBody(app, "POST", "/public/signup", string(body))
+
+		assert.Equal(t, http.StatusOK, r.Code)
+	})
+}
